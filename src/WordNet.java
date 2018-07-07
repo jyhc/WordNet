be078dbe2540c0c7cb9e306799e2d73da43e120e
Jack <jyhc40@gmail.com>
2018-07-06 17:31:09 -0700

    added SAP class

--- src/WordNet.java
+++ src/WordNet.java
@@ -2,17 +2,22 @@ import java.util.ArrayList;
 
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.RedBlackBST;
+import edu.princeton.cs.algs4.SET;
 //import edu.princeton.cs.algs4.ResizingArrayQueue;
 import edu.princeton.cs.algs4.StdOut;
 
 public class WordNet {
 	
-//    private final ResizingArrayQueue<String> lines;
-    public final ArrayList<String> lines = new ArrayList<String>();
-    public final Digraph wordGraph;
+    
+    private final RedBlackBST<String,SET<Integer>> nouns; //use RedBlackBST for searches in isNoun
+    private final ArrayList<String> lines; //used to store synsets
+    private final Digraph wordGraph;
     // constructor takes the name of the two input files
 	
     public WordNet(String synsets, String hypernyms) {
+        lines = new ArrayList<>();
+        nouns = new RedBlackBST<>();
         In sysnet = new In(synsets);
         In hypernym = new In(hypernyms);
 //        String line;
@@ -21,6 +26,17 @@ public class WordNet {
         while(!sysnet.isEmpty()) {
             parts = sysnet.readLine().split(",");
             lines.add(parts[1]);
+            
+            for(String s: parts[1].split(" ")) {
+                SET<Integer> value = nouns.get(s);
+                if(value == null) {
+                    value = new SET<>();//create new SET if not exist;
+                    value.add(Integer.parseInt(parts[0]));
+                    nouns.put(s, value);
+                } else {
+                    value.add(Integer.parseInt(parts[0]));
+                }  
+            }
         }
         wordGraph = new Digraph(lines.size());
         
@@ -35,19 +51,25 @@ public class WordNet {
         hypernym.close();
     }
     // returns all WordNet nouns
-//    public Iterable<String> nouns(){
-//        
-//    }
+    public Iterable<String> nouns(){
+        ArrayList<String> copy = new ArrayList<String>();
+        for(String s : lines) {
+            copy.add(s);
+        }
+        return copy;
+    }
 
     // is the word a WordNet noun?
-//    public boolean isNoun(String word) {
-//        
-//    }
+    public boolean isNoun(String word) {
+        /*nouns of a synset are separated by space; 
+          words only match if first word of noun matches word*/
+        return nouns.contains(word);
+    }
 
     // distance between nounA and nounB (defined below)
-//    public int distance(String nounA, String nounB) {
-//        
-//    }
+    public int distance(String nounA, String nounB) {
+        wordGraph.
+    }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
@@ -57,13 +79,16 @@ public class WordNet {
 
     // do unit testing of this class
     public static void main(String[] args) {
-        WordNet test = new WordNet("C:\\Users\\Test\\git\\WordNet\\wordnet-testing\\synsets.txt"
-                ,"C:\\Users\\Test\\git\\WordNet\\wordnet-testing\\hypernyms.txt");
+        WordNet test = new WordNet("testing\\synsets.txt"
+                ,"testing\\hypernyms.txt");
         StdOut.printf(test.lines.get(5115));
         StdOut.printf("\n");
         for(int item : test.wordGraph.adj(5115)) {
             StdOut.print(item);
         }
-
+//        for(String item : test.nouns()) {
+//            StdOut.print(item+"\n");
+//        }
+        StdOut.print(test.isNoun("175"));
     }
 }
